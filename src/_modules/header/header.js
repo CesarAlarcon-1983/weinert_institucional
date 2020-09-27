de110bace2058ce542abd0776437193845d9b7de
Your Name <you@example.com>
2020-09-27 17:33:56 -0300

    Updates

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -5,17 +5,78 @@ var Header = function() {
     var header = $('.header');
     var body = $('body');
     var menuOpen = $('.header__hamburguer');
-    var menuClose = $('.header__nav__close');
 
     menuOpen.on('click', function(){
-        header.addClass('-open');
-        body.addClass('-hideOverflow');
+        header.toggleClass('-open');
+        body.toggleClass('-hideOverflow');
     });
 
-    menuClose.on('click', function(){
-        header.removeClass('-open');
-        body.removeClass('-hideOverflow');
-    });
+    //Heder scroll logic
+    if(window.scrollY > 0) {
+        header.addClass('-scrolled');
+    } else {
+        header.removeClass('-scrolled');
+    }
+
+    $(window).on('scroll', function() {
+        if(window.scrollY > 0) {
+            header.addClass('-scrolled');
+        } else {
+            header.removeClass('-scrolled');
+        }
+    })
+
+    //Estilo data switcher logic
+    var contents = $('[data-content]');
+    var targets = $('[data-target]');
+    var closeButtons = $('.estilo__map__close-button');
+
+    closeButtons.on('click', function(e) {
+        e.preventDefault();
+        contents.removeClass('-active');
+    })
+
+    targets.on('click', function(e) {
+        e.preventDefault();
+
+        targets.removeClass('-active');
+        contents.removeClass('-active');
+
+        $(this).addClass('-active');
+
+        var targettedContent = $(this).data('target');
+
+        contents.filter(function(content) {
+            return content == targettedContent;
+        }).addClass('-active');
+    })
+
+    //Estilo horizontal scroll logic
+    var scrollControl = $('.estilo__map__scroll-button');
+    var container = $('.estilo__map__container');
+    var xMovement;
+
+    scrollControl.on('mousedown', function(e) {
+        var xInitialPosition = e.pageX;
+
+        $(document).on('mousemove', function(event) {
+            xMovement = xInitialPosition - event.pageX;
+            
+            if(xMovement <= 0) {
+                var currentScroll = container.scrollLeft();
+                container.scrollLeft(currentScroll - 10);
+            } else {
+                var currentScroll = container.scrollLeft();
+                container.scrollLeft(currentScroll + 10);
+            }
+
+            xInitialPosition =  event.pageX;
+        })
+    })
+
+    $(document).on('mouseup', function(e) {
+        $(this).unbind('mousemove');
+    })
 };
 
 module.exports = Header;
